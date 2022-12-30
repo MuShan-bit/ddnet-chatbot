894984cd30817a5243a00c81f965263c38d2e276
MuShan-bit <77890771+mushan-bit@users.noreply.github.com>
2022-12-30 16:47:24 +0800

    修改配置文件

--- src/main/java/xyz/mushan/simple2/Simple2Application.java
+++ src/main/java/xyz/mushan/simple2/Simple2Application.java
@@ -1,13 +1,17 @@
 package xyz.mushan.simple2;
 
+import love.forte.simbot.spring.autoconfigure.EnableSimbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableSimbot
+@EnableScheduling
 @SpringBootApplication
 public class Simple2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Simple2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Simple2Application.class, args);
+    }
 
 }
